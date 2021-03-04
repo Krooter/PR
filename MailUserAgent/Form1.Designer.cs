2f259fe2794d029a1c3f29a87d7b4e697dc1e0f6
Krooter <cristian.voroneanu@isa.utm.md>
2021-03-04 20:43:34 +0200

    LAB4

---
+++ MailUserAgent/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace MailUserAgent
+{
+    partial class LAB4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rtxtBody = new System.Windows.Forms.RichTextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtReceipient = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.rxtxReceive = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(96, 28);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(182, 20);
+            this.txtEmail.TabIndex = 0;
+            this.txtEmail.Text = "cpolesciuc99@gmail.com";
+            this.txtEmail.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(96, 68);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(182, 20);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.Text = "ga7UmKjEEt7jP5w";
+            this.txtPassword.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 68);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Password:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Email:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.rtxtBody);
+            this.groupBox1.Controls.Add(this.txtSubject);
+            this.groupBox1.Controls.Add(this.txtReceipient);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Location = new System.Drawing.Point(9, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(300, 389);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SEND";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 366);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "SEND";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 199);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Content";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 106);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Destinatar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 155);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Titlu";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // rtxtBody
+            // 
+            this.rtxtBody.Location = new System.Drawing.Point(22, 217);
+            this.rtxtBody.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rtxtBody.Name = "rtxtBody";
+            this.rtxtBody.Size = new System.Drawing.Size(256, 145);
+            this.rtxtBody.TabIndex = 6;
+            this.rtxtBody.Tag = "txtMail";
+            this.rtxtBody.Text = "TEXT";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(22, 171);
+            this.txtSubject.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(256, 20);
+            this.txtSubject.TabIndex = 5;
+            this.txtSubject.Text = "TEXT";
+            this.txtSubject.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // txtReceipient
+            // 
+            this.txtReceipient.Location = new System.Drawing.Point(22, 122);
+            this.txtReceipient.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtReceipient.Name = "txtReceipient";
+            this.txtReceipient.Size = new System.Drawing.Size(256, 20);
+            this.txtReceipient.TabIndex = 4;
+            this.txtReceipient.Tag = "txtReceipient";
+            this.txtReceipient.Text = "nicolaescucatalin2000@gmail.com";
+            this.txtReceipient.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.rxtxReceive);
+            this.groupBox2.Location = new System.Drawing.Point(324, 2);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(324, 389);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "RECEIVED";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(137, 366);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 19);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "RECEIVE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // rxtxReceive
+            // 
+            this.rxtxReceive.Location = new System.Drawing.Point(4, 19);
+            this.rxtxReceive.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rxtxReceive.Name = "rxtxReceive";
+            this.rxtxReceive.Size = new System.Drawing.Size(316, 344);
+            this.rxtxReceive.TabIndex = 6;
+            this.rxtxReceive.Text = "";
+            // 
+            // LAB4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 401);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "LAB4";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox rtxtBody;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.TextBox txtReceipient;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox rxtxReceive;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MailUserAgent/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+using S22.Imap;
+
+namespace MailUserAgent
+{
+    public partial class LAB4 : Form
+    {
+        static LAB4 f1;
+        public LAB4()
+        {
+            InitializeComponent();
+            f1 = this;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var message = new MailMessage(txtEmail.Text, txtReceipient.Text);
+            message.Subject = txtSubject.Text;
+            message.Body = rtxtBody.Text;
+            message.IsBodyHtml = true;
+
+            using (SmtpClient mailer = new SmtpClient("smtp.gmail.com", 587))
+            {
+                mailer.UseDefaultCredentials = false;
+                mailer.Credentials = new NetworkCredential(txtEmail.Text, txtPassword.Text);
+                mailer.EnableSsl = true;
+                mailer.Send(message);
+            }
+
+            txtSubject = null;
+            txtReceipient = null;
+            rtxtBody = null;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StartReceiving();
+        }
+
+        private void StartReceiving()
+        {
+            Task.Run(() =>
+            {
+                using (ImapClient client = new ImapClient("imap.gmail.com", 993,
+                    txtEmail.Text, txtPassword.Text, AuthMethod.Login, true))
+                {
+                    if(client.Supports("IDLE") == false)
+                    {
+                        MessageBox.Show("Server does not support IMAP file!");
+                        return;
+                    }
+                    client.NewMessage += new EventHandler<IdleMessageEventArgs>(OnNewMessage);
+                    while (true);
+                }
+            });
+        }
+
+        private void OnNewMessage(object sender, IdleMessageEventArgs e)
+        {
+            MessageBox.Show("Message received!");
+            MailMessage m = e.Client.GetMessage(e.MessageUID, FetchOptions.Normal);
+            f1.Invoke((MethodInvoker)delegate
+            {
+                f1.rxtxReceive.AppendText("From: " + m.From + "\n" +
+                                            "Subject:" + m.Subject + "\n"
+                                                + "Body:" + m.Body + "\n");
+            });
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
