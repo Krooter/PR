ae536833772eea6516d2786028e00a6ed91c5383
Krooter <cristian.voroneanu@isa.utm.md>
2021-03-11 12:15:42 +0200

    cors

--- dist/chat-server.js
+++ dist/chat-server.js
@@ -31,7 +31,24 @@ class ChatServer {
     }
     createApp() {
         this.app = require('cors');
-        this.app.use('cors');
+        this.app.use(function (req, res, next) {
+
+            // Website you wish to allow to connect
+            res.setHeader('Access-Control-Allow-Origin', 'https://kroochat.azurewebsites.net');
+        
+            // Request methods you wish to allow
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        
+            // Request headers you wish to allow
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        
+            // Set to true if you need the website to include cookies in the requests sent
+            // to the API (e.g. in case you use sessions)
+            res.setHeader('Access-Control-Allow-Credentials', true);
+        
+            // Pass to next layer of middleware
+            next();
+        });
         this.app = require('express');
     }
     createServer() {
